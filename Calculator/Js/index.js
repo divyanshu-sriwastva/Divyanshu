a89e84bdc8818edfcf59786cc9eaeaede683e28b
divyanshu-sriwastva <divyanshusriwastva@gmail.com>
2022-11-04 06:58:41 -0700

    Initial commit

---
+++ Calculator/Js/index.js
@@ -0,0 +1,47 @@
+let resultBox = document.getElementsByClassName("show-result")[0]
+let btn = document.getElementsByTagName("button")[0]
+btn.addEventListener("click", () => {
+    let exp = Array.from(document.getElementsByName("expression")[0].value)
+        let expCount = 0;
+        let errorMsg = document.getElementsByClassName("error")[0]
+        let fir_exp = exp.slice(0, exp.indexOf("+")) || exp.indexOf("+") || exp.indexOf("*") || exp.indexOf("/") || exp.indexOf("x") || exp.indexOf("X")
+        let sec_exp = exp.slice(exp.indexOf("+") + 1 || exp.indexOf("-") + 1 || exp.indexOf("*") + 1 || exp.indexOf("/") + 1 || exp.indexOf("x") + 1 || exp.indexOf("X") + 1,);
+        fir_exp = fir_exp.join("")
+        sec_exp = sec_exp.join("")
+        fir_exp = Number.parseFloat(fir_exp)
+        sec_exp = Number.parseFloat(sec_exp)
+        exp.map((val) => {
+            if (val == "+" || val == "-" || val == "*" || val == "/") {
+                expCount++;
+            }
+        })
+        if (exp.includes("+") || exp.includes("-") || exp.includes("*") || exp.includes("/") || exp.includes("x") || exp.includes("X")) {
+            if (expCount > 1) {
+                resultBox.innerHTML = "Sorry, i can't evaluate more than one term."
+                resultBox.style.fontSize = '15px'
+                errorMsg.hidden = false;
+                resultBox.style.color = "#000";
+            }
+            else {
+                if (exp.includes("+")) {
+                    resultBox.innerHTML = `= ${fir_exp + sec_exp}`
+                }
+                else if (exp.includes("-")) {
+                    resultBox.innerHTML = `= ${fir_exp - sec_exp}`
+                }
+                else if (exp.includes("*") || exp.includes("x") || exp.includes("X")) {
+                    resultBox.innerHTML = `= ${fir_exp * sec_exp}`
+                }
+                else if (exp.includes("/")) {
+                    resultBox.innerHTML = `= ${fir_exp / sec_exp}`
+                }
+                resultBox.style.fontSize = '18px'
+                errorMsg.hidden = true;
+                resultBox.style.color = "#000";
+            }
+        }
+        else {
+            resultBox.innerHTML = "Invalid"
+            resultBox.style.color = "rgb(163, 0, 8)";
+        }
+})
---
+++ Table/Js/script.js
@@ -0,0 +1,28 @@
+let fir = document.getElementById("fir-table")
+let sec = document.getElementById("sec-table")
+let thi = document.getElementById("thi-table")
+let fou = document.getElementById("fou-table")
+let fif = document.getElementById("fif-table")
+let six = document.getElementById("six-table")
+let sev = document.getElementById("sev-table")
+let eig = document.getElementById("eig-table")
+let nin = document.getElementById("nin-table")
+let body = document.getElementsByTagName("body")[0]
+let ten = document.getElementById("ten-table")
+let btn = document.getElementsByClassName("submit")[0]
+
+btn.addEventListener("click", () => {
+    let num = document.getElementsByName("number")[0].value
+    num = Number.parseFloat(num)
+    fir.innerHTML = `${num} x 1 = ${num * 1}`
+    sec.innerHTML = `${num} x 2 = ${num * 2}`
+    thi.innerHTML = `${num} x 3 = ${num * 3}`
+    fou.innerHTML = `${num} x 4 = ${num * 4}`
+    fif.innerHTML = `${num} x 5 = ${num * 5}`
+    six.innerHTML = `${num} x 6 = ${num * 6}`
+    sev.innerHTML = `${num} x 7 = ${num * 7}`
+    eig.innerHTML = `${num} x 8 = ${num * 8}`
+    nin.innerHTML = `${num} x 9 = ${num * 9}`
+    ten.innerHTML = `${num} x 10 = ${num * 10}`
+    body.style.height = '100%'
+})
